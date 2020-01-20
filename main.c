43016b33f5c5879e9313223421572dee24e6db14
The Geek on Skates <geek@geekonskates.com>
2020-01-20 00:52:20 -0500

    Starting to look like a text box
    The cursor stays inside the window when you hit backspace and you're at the beginning.
    It still overruns the right side, and all the great shortcuts (home/end, selection,
    etc.) hasn't been implemented yet, but what I have here is a start.

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+#include <stdlib.h>
 #include "nc-windows.h"
 #include "say.h"
 
 WINDOW *path, *list;
+unsigned int cursor;
+unsigned int max = 50;
+char *text;
 
 int main(int argc, char *argv[]) {
 	initscr();
@@ -13,10 +17,42 @@ int main(int argc, char *argv[]) {
 	list = create_window(0, 3, COLS, LINES - 4);
 	mvaddstr(LINES - 1, 0, "Press F1 for a menu.");
 	move(1, 1);
-	say("What a geek", 0);
 	
-	getch();
+	text = malloc(50);
+	if (text == NULL) {
+		perror("Error starting program: malloc failed.\n");
+		return 1;
+	}
 	
+	int ch;
+	while(ch = getch()) {
+		if (ch == KEY_F(1)) break;
+		if (ch == KEY_BACKSPACE) {
+			say("backspace", SAY_NOW | SAY_ASYNC);
+			text[cursor] = 0;
+			if (cursor == 0) {
+				move(1, 1);
+			}
+			else {
+				cursor--;
+			}
+		}
+		else {
+			text[cursor] = ch;
+			cursor++;
+			if (cursor == max) {
+				text = realloc(text, max + 10);
+				max += 10;
+			}
+		}
+		move(1, 1);
+		for (int i=0; i<COLS - 4; i++)
+			mvaddch(1, 1 + i, ' ');
+		mvprintw(1, 1, text);
+		wrefresh(path);
+	}
+	
+	free(text);
 	destroy_window(path);
 	destroy_window(list);
 	endwin();
